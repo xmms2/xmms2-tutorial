e40860b1ed798b3e493f66c589a01e468db48b20
Georg Schild <dangertools@gmail.com>
2006-07-02 12:51:37 +0200

    FEATURE(925): fix small JutorialCleitn bug

--- java/org/xmms2/tutorial/jutorialClient/JutorialMlibNode.java
+++ java/org/xmms2/tutorial/jutorialClient/JutorialMlibNode.java
@@ -82,14 +82,11 @@ public class JutorialMlibNode extends DefaultMutableTreeNode {
 		"m1.id = m2.id WHERE m1.key='artist' " +
 		"AND m1.value LIKE '%" + name + "%' AND m2.key='album'";
 		case 2: return "SELECT DISTINCT m1.id, ifnull(m1.value,'[unknown]') as artist, " +
-		"ifnull(m2.value,'[unknown]') as album, " +
-		"ifnull(m5.value, m6.value) as title, m7.value as tracknr, m4.value " +
-		"as art from Media m1 left join Media m2 on m1.id = m2.id and m2.key='album' and m2.value " +
-		"LIKE '%" + name + "%' left join Media m4 on m4.id = m1.id and m4.key='album_front_small' " +
-		"left join Media m5 on m1.id = m5.id and m5.key='title' left join Media m6 on m1.id = " +
-		"m6.id and m6.key='url' left join Media m7 on m1.id = m7.id and m7.key='tracknr' " +
-		"where m1.key='artist' and m1.value like " +
-		"'%" + parent + "%' order by m1.id, artist, album, tracknr, title";
+		"ifnull(m2.value,'[unknown]') as album, ifnull(m5.value, m6.value) as title from " +
+		"Media m1 left join Media m2 on m1.id = m2.id and m2.key='album' left join Media " +
+		"m5 on m1.id = m5.id and m5.key='title' left join Media m6 on m1.id = m6.id and " +
+		"m6.key='url' where m1.key='artist' and m1.value like '%" + parent + "%' and " +
+		"m2.value LIKE '%" + name + "%' order by m1.id, artist, album, title";
 		default: return "";
 		}
 	}
